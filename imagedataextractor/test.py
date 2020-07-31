32e287157066a4e9eeef0e61028d145d6067ddc1
Batuhan <by256@cam.ac.uk>
2020-07-31 18:51:42 +0100

    text detection in progress

--- imagedataextractor/test.py
+++ imagedataextractor/test.py
@@ -10,22 +10,5 @@ image = Image.open('../examples/10nm.png')
 image = np.array(image)
 h, w = image.shape[:2]
 
-image = image[h - h//5:, w - w//5:]
-h, w = image.shape[:2]
-image = Image.fromarray(image).resize((w*3, h*3), resample=Image.BICUBIC)
-
-
-boxes = detect_bounding_boxes(image)
-
-# a, b, c, d = boxes[0]
-
-# print(a)
-# print(h - b)
-# print(c)
-# print(h - d)
-
-# image = cv2.rectangle(image, (w-a, h-b, c, d), (0, 255, 0), 2)
-plt.imshow(image)
-plt.show()
-
-print(boxes)
+detector = TextDetector()
+boxes = detector.detect_text(image)
