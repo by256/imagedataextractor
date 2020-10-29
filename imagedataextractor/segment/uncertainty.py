a93ddcaca28fc02ad38e17ccbd5c3bf9075e8d34
by256 <by256@cam.ac.uk>
2020-10-29 09:03:22 +0000

    add uncertainty

---
+++ imagedataextractor/segment/uncertainty.py
@@ -0,0 +1,26 @@
+import torch
+
+
+def entropy(p, eps=1e-6):
+    p = torch.clamp(p, eps, 1.0-eps)
+    return -1.0*((p*torch.log(p)) + ((1.0-p)*(torch.log(1.0-p))))
+
+def expected_entropy(mc_preds):
+    return torch.mean(entropy(mc_preds), dim=0)
+
+def predictive_entropy(mc_preds):
+    return entropy(torch.mean(mc_preds, dim=0))
+
+def uncertainty_filtering(prediction, uncertainty, t=0.0125):
+
+    filtered_pred = torch.zeros_like(prediction)
+
+    for inst_id in torch.unique(prediction):
+        if inst_id == 0:
+            continue
+        inst_mask = prediction == inst_id
+        inst_uncertainty = torch.mean(uncertainty[inst_mask])
+        if inst_uncertainty < t:
+            filtered_pred[inst_mask] = torch.max(filtered_pred) + 1
+
+    return filtered_pred
