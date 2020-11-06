b89375babd2b68d41dd07cd2ef3736bcce72be16
by256 <by256@cam.ac.uk>
2020-11-06 09:34:20 +0000

    add message when scalebar not found

--- imagedataextractor/extract.py
+++ imagedataextractor/extract.py
@@ -56,6 +56,8 @@ def extract_image(image, target_dir, bayesian=True, min_particles=10, device='cp
         cv2.putText(output_image, text, (image.shape[1]//2, image.shape[0]//2), cv2.FONT_HERSHEY_SIMPLEX, 1, color=(24, 24, 205))
         cv2.rectangle(scalebar_image, (x, y), (x + w, y + h), (24, 24, 205), 1)
         cv2.putText(scalebar_image, text, (image.shape[1]//2, image.shape[0]//2), cv2.FONT_HERSHEY_SIMPLEX, 1, color=(24, 24, 205))
+    else:
+        cv2.putText(scalebar_image, 'Scalebar not found.', (image.shape[1]//2, image.shape[0]//2), cv2.FONT_HERSHEY_SIMPLEX, 1, color=(24, 24, 205))
 
     # segment particles
     particle_preds, uncertainty, original = segmenter.segment(image)
