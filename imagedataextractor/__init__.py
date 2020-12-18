3dfc560da1eae690679571ab2bc9073f2e61f8b6
by256 <by256@cam.ac.uk>
2020-12-18 10:30:41 +0000

    add docstring and package metadata to base __init__.py

--- imagedataextractor/__init__.py
+++ imagedataextractor/__init__.py
@@ -1,5 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+ImageDataExtractor
+Microscopy image quantification.
+by256@cam.ac.uk
+~~~~~~~~~~~~~~~
+"""
+
 import logging
 from .extract import *
 from .figsplit import figsplit
+
+__title__ = 'ImageDataExtractor'
+__version__ = '2.0.0'
+__author__ = 'Batuhan Yildirim'
+__email__ = 'by256@cam.ac.uk'
+__license__ = 'MIT License'
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(levelname)s : %(message)s')
