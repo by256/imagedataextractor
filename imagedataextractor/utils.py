e7f73021ce9b74ce2c17064552cad25cbfbf5092
by256 <by256@cam.ac.uk>
2021-01-05 14:32:45 +0000

    add visualise_seg

--- imagedataextractor/utils.py
+++ imagedataextractor/utils.py
@@ -5,8 +5,13 @@ General utils.
 .. codeauthor:: Batuhan Yildirim <by256@cam.ac.uk>
 """
 
+import os
 import cv2
+import copy
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
 def get_contours(x):
@@ -28,3 +33,15 @@ def shuffle_segmap(x):
         shuffled_segmap[inst_mask] = i + 1
         
     return shuffled_segmap
+
+def visualize_seg(data, save_to):
+    seg = data.segmentation.astype(float)
+
+    seg_cmap = copy.copy(matplotlib.cm.tab20)
+    seg_cmap.set_bad(color='k')  # black
+
+    if len(np.unique(seg)) > 1:
+        seg[seg == 0.0] = np.nan
+        plt.imsave(os.path.join(save_to, '{}-seg.png'.format(data.fn)), seg, cmap=seg_cmap)
+    else:
+        plt.imsave(os.path.join(save_to, '{}-seg.png'.format(data.fn)), seg, cmap='gray')
