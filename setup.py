bf48e15429b59151ae2e1a6e35d36ddb3305db7e
by256 <by256@cam.ac.uk>
2021-04-13 15:17:05 +0100

    update version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('./README.md', 'r') as f:
 
 setuptools.setup(
     name='imagedataextractor',
-    version='2.0.0',
+    version='2.0.1',
     description='imagedataextractor is a Python library for electron microscopy image quantification.',
     long_description=long_description,
     long_description_content_type='text/markdown',
